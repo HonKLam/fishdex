8ac7ef5ed15aeaeefc368b48c93306ecae53ca43
Lamo <62069795+HonKLam@users.noreply.github.com>
2024-03-26 22:02:07 +0100

    Backend Hinzufügen

--- fd-backend/src/main/java/dev/hklm/fdbackend/TestController.java
+++ fd-backend/src/main/java/dev/hklm/fdbackend/TestController.java
@@ -1,2 +1,14 @@
-package dev.hklm.fdbackend;public class TestController {
+package dev.hklm.fdbackend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
 }
