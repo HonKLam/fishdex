0e8a29e434caa3cba9c40ce54d7c3431b23e4070
Lamo <62069795+HonKLam@users.noreply.github.com>
2024-03-30 00:05:06 +0100

    Add Fishdex API Endpoint (GET)

--- fd-backend/src/main/java/dev/hklm/fdbackend/Application.java
+++ fd-backend/src/main/java/dev/hklm/fdbackend/Application.java
@@ -1,10 +1,25 @@
 package dev.hklm.fdbackend;
 
+import dev.hklm.fdbackend.Entities.Fish;
+import dev.hklm.fdbackend.Entities.Fishdex;
+import dev.hklm.fdbackend.Entities.TestEntity;
+import dev.hklm.fdbackend.Repositories.FishdexRepository;
+import dev.hklm.fdbackend.Repositories.TestRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
 public class Application {
+
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
--- fd-backend/src/main/java/dev/hklm/fdbackend/Controllers/TestController.java
+++ fd-backend/src/main/java/dev/hklm/fdbackend/Controllers/TestController.java
@@ -29,4 +29,3 @@ public class TestController {
     }
 
     // TODO - Test for POST
-}
