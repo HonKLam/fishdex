34b424fe616028c1aa516ada7bbb036925b975a1
Strößner <stroessner@ivi.fraunhofer.de>
2024-04-13 17:26:41 +0200

    created catch service file, w.i.p.

---
+++ fd-backend/src/main/java/dev/hklm/fdbackend/Services/CatchService.java
@@ -0,0 +1,24 @@
+package dev.hklm.fdbackend.Services;
+import dev.hklm.fdbackend.Entities.Fish;
+import dev.hklm.fdbackend.Entities.Fishdex;
+import dev.hklm.fdbackend.Repositories.FishdexRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+
+
+@Service
+public class CatchService {
+
+    // TODO:
+    // Was muss hier rein?
+    // CatchController-Zeugs
+    // Zeugs aus dem CatchController hier rein verschieben
+    // dann vom Catch-Controller das Zeug hier drin aufrufen
+    // Vergleiche dafür: FishController + FishService
+
+
+}
